8e79264c283e2add7fc7d6d2fa5e9c18cda8f02f
Martin Wåger <martin.w@comaround.se>
2014-07-26 22:39:35 +0200

    Initial with some modifiactions

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sportsstoreApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the sportsstoreApp
+ */
+angular.module('sportsstoreApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sportsstoreApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sportsstoreApp
+ */
+angular.module('sportsstoreApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
