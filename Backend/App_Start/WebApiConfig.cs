788aecb2b4356fd22769e73ee89138669a917d5d
Martin Wåger <martin.w@comaround.se>
2014-07-29 00:06:43 +0200

    Added call to backend. Added cors support for backend

--- Backend/App_Start/WebApiConfig.cs
+++ Backend/App_Start/WebApiConfig.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Formatting;
 using System.Net.Http.Headers;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace Backend
 {
@@ -13,6 +13,8 @@ namespace Backend
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
 
             // Web API routes
             config.MapHttpAttributeRoutes();
