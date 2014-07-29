8d124b332b9a6340be5ea00f8405d9158eed1c9f
Martin Wåger <martin.w@comaround.se>
2014-07-29 23:02:22 +0200

    Fixed error handling and JSHint errors

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,14 +1,14 @@
 'use strict';
 
-angular.module("sportsStore")
-    .constant("dataUrl", "http://localhost:56322/api/products")
-    .controller("sportsStoreCtrl", function ($scope, $http, dataUrl) {
+angular.module('sportsStore')
+    .constant('dataUrl', 'http://localhost:56322/api/products')
+    .controller('sportsStoreCtrl', function ($scope, $http, dataUrl) {
         $scope.data = {};
         $http.get(dataUrl)
             .success(function (data) {
                 $scope.data.products = data;
             })
-            .error(function (error) {
-                $scope.data.error = error;
+            .error(function (data, status) {
+                $scope.data.error = status;
             });
     });
